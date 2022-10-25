6d7cf73d9ff2d0c429e08cede46b5fbb4e68259c
Nataly Carvalho <natalycarvalho.dev@gmail.com>
2022-10-25 19:51:28 -0300

    Update serve.js

--- projetoBD/serve.js
+++ projetoBD/serve.js
@@ -1,39 +1,32 @@
 const express = require('express');
 //const { MongoClient } = require('mongodb');
 const app = express();
+const {MongoClient}= require('mongodb-legacy')
+const uri = "mongodb+srv://dbUser:dbUser@cluster0.xnmmqjo.mongodb.net/?retryWrites=true&w=majority";
+//email educacional que a conta do mongo  foi criada
+const client = new MongoClient(uri);
+const db = client.db("teste-db");
+const collection = db.collection('crud');
 
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://dbUser:aluno@cluster0.zirndln.mongodb.net/?retryWrites=true&w=majority";
-
-
-MongoClient.connect(uri,(err, client)=>{
-    if(err) return console.log(err)
-    db = client.db('teste-bd')
-
-    app.listen(3000, ()=>{
-        console.log("Servidor rodando tranquilo")
-    })
-})
-
-// app.listen(3000, function(){
-//     console.log('o nosso servidor esta rodando na porta 3000')
-// }) 
+ app.listen(3000, function(){
+    console.log('o nosso servidor esta rodando na porta 3000')
+ }) 
 
 app.get('/',(req, res)=>{
-    res.render('index.ejs')
+    res.send('index.ejs')
 })
 app.set('view engine', 'ejs')
 
 app.use(express.urlencoded({extended:true}))
 
 app.post('/show',(req, res)=>{
-   db.conection('crud').insertOne(req.body,(err,result)=>{
+   db.collection('crud').insertOne(req.body,(err,result)=>{
     if(err) return console.log(err)
     console.log("salvou no banco de dados mongodb")
     res.redirect('/show')
-    db.collection('crud').find().toArray((err,result)=>{
-        console.log(result)
+    db.collection('crud').find().toArray((err,results)=>{
+        console.log(results)
     })
    })
 })
