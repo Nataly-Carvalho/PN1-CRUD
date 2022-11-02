65b4b42363caea6c77e1f7e261ab6c3c53be6758
Nataly <natalycarvalho404@gmail>
2022-11-01 23:02:34 -0300

    Crud completo

--- projetoBD/serve.js
+++ projetoBD/serve.js
@@ -1,6 +1,7 @@
 const express = require('express');
 //const { MongoClient } = require('mongodb');
 const app = express();
+const ObjectId = require('mongodb-legacy').ObjectId
 const {MongoClient}= require('mongodb-legacy')
 const uri = "mongodb+srv://dbUser:dbUser@cluster0.xnmmqjo.mongodb.net/?retryWrites=true&w=majority";
 //email educacional que a conta do mongo  foi criada
@@ -30,3 +31,57 @@ app.post('/show',(req, res)=>{
     })
    })
 })
+
+//renderizar e retornar o conteúdo do nosso banco
+app.get('/', (req, res) => {
+    let cursor = db.collection('crud').find()
+})
+
+//renderizar e retornar o conteúdo do nosso banco
+app.get('/show', (req, res) => {
+    collection.find().toArray((err, results) => {
+        if (err) return console.log(err)
+        res.render('show.ejs', {crud: results})
+    })
+})
+
+//criando a nossa rota e comandos para editar
+app.route('/edit/:id')
+.get((req, res) => {
+    var id = req.params.id
+
+    collection.find(ObjectId(id)).toArray((err, result) => {
+        if(err) return res.send(err)
+        res.render('edit.ejs', {crud: result})
+    })
+})
+.post((req, res) => {
+    var id = req.params.id
+    var name = req.body.name
+    var surname = req.body.surname
+
+    collection.updateOne({_id: ObjectId(id)}, {
+        $set: {
+            name: name,
+            surname: surname
+        }
+    }, (err, result) => {
+        if(err) return res.send(err)
+        res.redirect('/show')
+        console.log('Banco de dados atualizado')
+
+
+    })
+})
+ app.route('/delete/:id')
+ .get((req,res)=>{
+    var id= req.params.id
+    db.collection('crud').deleteOne({_id: ObjectId(id)}, (err, result)=>{
+        if(err) return res.send(500,err)
+        console.log('Deletando do nosso banco de dados!')
+        res.redirect('/show')
+    })
+ })
+
+
+
